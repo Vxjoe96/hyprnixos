2bdab2d82eb8544de4b252cc60326aaed4cd2e26
Vxjoe96 <josephtspringer@gmail.com>
2024-04-01 17:13:40 -0600

    Update home.nix

--- home.nix
+++ home.nix
@@ -1,31 +1,53 @@
-{ config, pkgs, ... }:
-
-let
-  colloid_name = "Colloid";
-  colloid = pkgs.colloid-gtk-theme.override {
-    themeVariants = [ "grey" ];
-    tweaks = [ "rimless" "black" ];
-    
-  };
-in
 {
-  
-gtk = {
-    enable = true;
-    theme = {
-      name = colloid_name;
-      package = colloid;
-    };  
-};
-
- qt = {
-    enable = true;
-    platformTheme = "gtk";
+  inputs,
+  pkgs,
+  config,
+  ...
+}: {
+  home.pointerCursor = {
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Original-Classic";
+    size = 16;
+    gtk.enable = true;
+    x11.enable = true;
   };
-  
-  home.packages = with pkgs; [
-    xfce.thunar
-  ];
+
+  gtk = {
+    enable = true;
+    font = {
+      name = "AestheticIosevka Nerd Font";
+      size = 9;
+    };
+
+    gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+
+    iconTheme = {
+      name = "WhiteSur";
+      package = pkgs.whitesur-icon-theme;
+    };
+
+    theme = {
+       name = "Colloid-Dark";
+       package = pkgs.colloid-gtk-theme.override {
+         tweaks = ["rimless" "black" "float"];
+       };
+
+    gtk3.extraConfig = {
+      gtk-xft-antialias = 1;
+      gtk-xft-hinting = 1;
+      gtk-xft-hintstyle = "hintslight";
+      gtk-xft-rgba = "rgb";
+      gtk-application-prefer-dark-theme = 1;
+    };
+    gtk2.extraConfig = ''
+      gtk-xft-antialias=1
+      gtk-xft-hinting=1
+      gtk-xft-hintstyle="hintslight"
+      gtk-xft-rgba="rgb"
+    '';
+
+    gtk4.extraConfig = {gtk-application-prefer-dark-theme = 1;};
+  };
   
   # TODO please change the username & home directory to your own
   home.username = "joseph";
